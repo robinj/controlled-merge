47a51d5cc893d62611b5221bb994474172a0a61b
Keith Chester <kchester@gmail.com>
2015-07-10 11:06:20 -0500

    feature: merge function written
    Initial writing of the basic idea for the library. Tests need to be
    written.

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = function(){
+	var self = this;
+	
+	var toMerge = Array.prototype.slice.call(arguments);
+	if(toMerge.length == 0) throw new Error('controlled-merge must be called with at least one object');
+	
+	var onConflict;
+	if(typeof toMerge[0] == 'function') onConflict = toMerge.shift();
+	if(toMerge.length == 1) return toMerge[0];
+	
+	var results = toMerge.shift();
+	
+	var iterateAndMerge = function(obj1, obj2){
+		for(var attr in obj2){
+			if(!obj1[attr]){
+				obj1[attr] = obj2[attr];
+			} else if(typeof obj1[attr] == 'object' && typeof obj2[attr] == 'object'){
+				obj1[attr] = iterateAndMerge(obj1[attr], obj2[attr]);
+			} else if(onConflict){
+				obj1[attr] = onConflict(obj1[attr], obj2[attr]);
+			} else {
+				obj1[attr] = obj2[attr];
+			}
+		}
+		
+		return obj1;
+	};
+	
+	toMerge.forEach(function(next){
+		results = iterateAndMerge(results, next);
+	});
+	
+	return results;
+}
